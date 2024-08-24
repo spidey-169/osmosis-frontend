949637967b6ac3644e7789c91ac8430275877af4
BlackBlocks <info@blackblocks.io>
2024-08-24 22:56:07 +0200

    modified:   packages/web/next.config.js

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -3,8 +3,9 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   reactStrictMode: true,
   images: {
-    domains: ["app.osmosis.zone"],
-    unoptimized: true, 
+    loader: 'default', // Verwendet den Standard-Loader für Bilder
+    path: '/',
+    unoptimized: true,
   },
   enabled: process.env.ANALYZE === "true",
 });
