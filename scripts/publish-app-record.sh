708ffcc3c7fffde7ff35bfeff06131edf2ad7b0c
BlackBlocks <info@blackblocks.io>
2024-08-25 00:14:42 +0200

    modified:   .env 	modified:   scripts/publish-app-record.sh

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -95,7 +95,7 @@ record:
   config:
     env:
       CERC_WEBAPP_DEBUG: "$rcd_app_version"
-      CERC_BUILD_OUTPUT_DIR: "."
+      CER_BUILD_TOOL: "yarn"
   meta:
     note: "Added by CI @ `date`"
     repository: "`git remote get-url origin`"
