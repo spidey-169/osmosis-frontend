f324670352f2426e08d35475c1dcb6c5ab04030c
BlackBlocks <info@blackblocks.io>
2024-08-24 10:24:00 +0200

    modified:   scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,22 +9,22 @@ lerna run build
 
 # Erstelle das Output-Verzeichnis, falls es nicht existiert
 mkdir -p $OUTPUT_DIR
-mkdir -p $OUTPUT_DIR/packages
+#mkdir -p $OUTPUT_DIR/packages
 
 # Verschiebe alle Build-Artefakte in das Output-Verzeichnis
 # Dies hängt davon ab, wie deine Artefakte strukturiert sind
 # Hier ein Beispiel für eine allgemeine Verschiebung
 
 # Verschiebe Inhalte aus den verschiedenen Verzeichnissen
-#mv packages/* $OUTPUT_DIR/packages/
+mv packages/web $OUTPUT_DIR
 #mv lerna.json $OUTPUT_DIR/
 #mv package.json $OUTPUT_DIR/
 
 
-for item in *; do
-    if [ "$item" != "build" ]; then
-        mv "$item" build/
-    fi
-done
+#for item in *; do
+#    if [ "$item" != "build" ]; then
+#        mv "$item" build/
+#    fi
+#done
 
 echo "Build abgeschlossen. Artefakte befinden sich in $OUTPUT_DIR."
